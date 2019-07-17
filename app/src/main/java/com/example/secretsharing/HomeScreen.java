1405fda38f0c79338013d4b0a2dc3b131671e3cf
kickstv <kicksmedia1@gmail.com>
2019-07-17 10:59:20 +0100

    improving layout of app

--- app/src/main/java/com/example/secretsharing/HomeScreen.java
+++ app/src/main/java/com/example/secretsharing/HomeScreen.java
@@ -1,23 +1,28 @@
 package com.example.secretsharing;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeScreen extends AppCompatActivity {
 
     private static final String TAG = "HomeScreen";
     private SectionsPageAdapter mSectionsPageAdapter;
     private ViewPager mViewPager;
+    private TextView username;
+    private TabLayout tabNav;
+    private ImageView settingIcon;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -26,40 +31,29 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.home_screen);
         Log.d(TAG, "onCreate: Starting.");
 
-        String account = getIntent().getStringExtra("account");
 
-        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        // set username
+        setUserName();
 
+
+        // Setting up tab navigation
         mViewPager = (ViewPager) findViewById(R.id.container);
         setupViewPager(mViewPager);
-
-
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
         // Navigation Tabs
-        TabLayout tabNav = (TabLayout)findViewById(R.id.tabs);
+        tabNav = (TabLayout)findViewById(R.id.tabs);
         tabNav.setupWithViewPager(mViewPager);
 
 
         // Settings icon button
-        ImageView image = (ImageView)findViewById(R.id.settingsIcon);
-        image.setOnClickListener(new View.OnClickListener() {
+        settingIcon = (ImageView)findViewById(R.id.settingsIcon);
+        settingIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(HomeScreen.this, settingsScreen.class); // redirecting to settings_screen.
                 startActivity(intent);
             }
         });
-
-
-
-
-
-
-
-
-
-
-
-
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -68,5 +62,17 @@ public class HomeScreen extends AppCompatActivity {
         adapter.addFragment(new secretImageFragment(), "Secret Image");
 
         viewPager.setAdapter(adapter);
+    }
+
+    private void setUserName() {
+        username = findViewById(R.id.txt_username);
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            // already signed in
+            FirebaseUser user = mAuth.getCurrentUser();
+
+            username.setText(user.getDisplayName());
+        }
     }
 }
--- app/src/main/java/com/example/secretsharing/secretCodeFragment.java
+++ app/src/main/java/com/example/secretsharing/secretCodeFragment.java
@@ -7,13 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -21,23 +24,20 @@ import java.util.ArrayList;
 public class secretCodeFragment extends Fragment {
 
     private static final String TAG = "SecretCodeFragment";
-
     String MyPREFERENCES = "MyPrefs" ;
-
-    SharedPreferences sharedpreferences;
-
+    private SharedPreferences sharedpreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.secretcode_fragment, container,false);
 
+        // Getting activity components
         final EditText secretCodeInput = (EditText)view.findViewById(R.id.secretCode);
-        final Button showButton = (Button)view.findViewById(R.id.showButton);
         final TextView output = (TextView)view.findViewById(R.id.shareOutput);
-        final Button hideButton = (Button)view.findViewById(R.id.hideButton);
+        final Spinner mspin= (Spinner)view.findViewById(R.id.numberOfShares);
+        final ImageView lockIcon = (ImageView)view.findViewById(R.id.img_lock);
 
-        final Spinner mspin = (Spinner)view.findViewById(R.id.numberOfShares);
-
+        // Setting up spinner
         Integer[] items = new Integer[]{2,3,4,5,6};
         ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getActivity(), android.R.layout.simple_spinner_item, items);
         mspin.setAdapter(adapter);
@@ -46,34 +46,47 @@ public class secretCodeFragment extends Fragment {
         sharedpreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
         String share1 = sharedpreferences.getString("share1", "none");
 
+
+        // Checking if share exists
         if (!share1.equals("none")) {
+            Picasso.get().load("https://image.flaticon.com/icons/svg/25/25239.svg").into(lockIcon, new Callback() {
+
+                @Override
+                public void onSuccess() {
+                    System.out.println("Lock image loaded successfully");
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    System.out.println("Lock image loaded unsuccessfully");
+                    e.printStackTrace();
+                }
+            });
             toggleButton(view);
             output.setText(share1);
         }
 
 
-        showButton.setEnabled(false);
 
-        hideButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+        lockIcon.setOnClickListener(v -> {
 
-                Secret s = new Secret();
+            Secret s = new Secret();
 
-                if (secretCodeInput.getText().toString().length() > 0) {
-                    if (secretCodeInput.getText().toString().matches("[a-zA-Z0-9]*")) {
-                        ArrayList<String> result = s.hideSecret(secretCodeInput.getText().toString());
-                        if (result != null) {
-                            output.setText(result.get(0));
-                            toggleButton(view);
-                            storeLocalShare(result.get(0));
-                        }
-                    }else {
-                        Toast.makeText(getActivity(), "Please remove special characters!", Toast.LENGTH_SHORT).show();
+            if (secretCodeInput.getText().toString().length() > 0) {
+                if (secretCodeInput.getText().toString().matches("[a-zA-Z0-9]*")) {
+                    ArrayList<String> result = s.hideSecret(secretCodeInput.getText().toString());
+                    if (result != null) {
+
+                        Picasso.get().load(R.drawable.ic_padlock_red).into(lockIcon);
+                        output.setText(result.get(0));
+                        toggleButton(view);
+                        storeLocalShare(result.get(0));
                     }
                 }else {
-                    Toast.makeText(getActivity(), "Field cannot be left empty!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Please remove special characters!", Toast.LENGTH_SHORT).show();
                 }
+            }else {
+                Toast.makeText(getActivity(), "Field cannot be left empty!", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -96,20 +109,13 @@ public class secretCodeFragment extends Fragment {
 
     public void toggleButton(View view) {
         final EditText secretCodeInput = (EditText)view.findViewById(R.id.secretCode);
-        final Button showButton = (Button)view.findViewById(R.id.showButton);
-        final Button hideButton = (Button)view.findViewById(R.id.hideButton);
         final Spinner mspin = (Spinner)view.findViewById(R.id.numberOfShares);
 
 
         secretCodeInput.setText("");
         secretCodeInput.setVisibility(View.INVISIBLE);
         secretCodeInput.setEnabled(false);
-        hideButton.setEnabled(false);
-        showButton.setEnabled(true);
         mspin.setEnabled(false);
         mspin.setVisibility(View.INVISIBLE);
-    }
-    public void shareExists() {
-
     }
 }
